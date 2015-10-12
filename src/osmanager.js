efd2d9d2e04e2f69a4ae3dea6536c787a28339c0
EduardoMY <eduardo.medina@cetys.edu.mx>
2015-10-12 01:48:43 -0700

    PCB complete

--- src/osmanager.js
+++ src/osmanager.js
@@ -25,6 +25,7 @@ class Process{
 	
 	this.id=id;
 	this.arrival=arrival;
+	this.arrivalIO=0;
 
 	this.cpuCounter=0;
 	this.ioTime=0;
@@ -46,22 +47,33 @@ class Process{
 	}
 
 	Print(){ //only gives an array with the data
-		//format: ID|Arrival|CPU nedded| CPU Ussage| IO TIme | TIme of IO| Finish Time | Time in the System
+		//format: ID|Arrival|CPU nedded| CPU Ussage| IO TIme | Time of IO| Arrival Printer| 
+		//	Waiting Time |  Time in the System | Finish Time| Status
 		var data=[];
+		var status;
 		data.push(this.id); //ID
 		data.push(this.arrival); //Arival
 		data.push(this.cpuTimeNedded); //CPU nedded
 		data.push(this.cpuTime); //CPU Ussage
 		data.push(this.ioTimeNedded); //IO TIme nedded
 		data.push(this.ioTime); //IO Time Ussage
+		data.push(this.arrivalIO);
+		data.push(this.waitingTime);
 		if(this.terminated){
-			data.push(this.TotalTime() + this.arrival); // Time the process ended
 			data.push(this.TotalTime()); // Time in the SYstem
+			data.push(this.TotalTime() + this.arrival); // Time the process ended
+			
+			if(this.isDone())
+				status="Finished";
+			else 
+				status="Aborted"
 		}
 		else {
-			data.push(Number.NaN);
-			data.push(Number.NaN);
+			data.push(0);
+			data.push(0);
+			status="InSystem"
 		}
+		data.push(status); //Status: Finished or Aborted
 		return data;
 	}
 }
@@ -434,6 +446,7 @@ function waiting_IO(){
 		eProcess=waitingList.Remove();
 		deleteFirstRow("waiting");
 		ioList.Push(eProcess);
+		eProcess.arrivalIO=time;
 		addRowsEnd("io", eProcess);
 	}
 
@@ -509,8 +522,13 @@ function updatePCB() {
 
 
 function modifyPCBRow(cells, actions){
+	var actualAction;
 	for (var i = actions.length - 1; i >= 0; i--) {
-		cells[i].innerHTML=actions[i];
+		actualAction=actions[i];
+		if(actualAction==0)
+			cells[i].innerHTML=" - ";
+		else 
+			cells[i].innerHTML=actualAction;
 	};
 }
 
